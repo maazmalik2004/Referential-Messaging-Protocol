1e0a3d665c4d15564e1c12f49453df8bc03ced57
Maaz Malik <maazmalik2004@gmail.com>
2026-01-08 23:23:47 +0530

    [CHECKPOINT] sender side working

--- protocol/RMP.js
+++ protocol/RMP.js
@@ -1,6 +1,5 @@
 import { EventEmitter } from "node:events";
 import utils from "../utils/utils.js";
-import { copyFile } from "node:fs";
 
 class RMP{
     constructor(object){
@@ -12,11 +11,22 @@ class RMP{
         this.adapter.on("message",message=>{
             console.log("[RMP] received message ",message)
         })
+
+        this.adapter.on("connected",()=>{
+            this.emitter.emit("connected");
+        })
+
+        this.adapter.on("disconnected",()=>{
+            this.emitter.emit("disconnected");
+        })
+
+        this.adapter.on("dropped",(message)=>{
+            this.emitter.emit("dropped", message);
+        })
     }
 
     parse(object,currentPath){
         if(this.isPrimitive(object)){
-            // console.log(object);
             this.hashValueMapping.set(utils.getHash(currentPath),object)
             return;
         }
@@ -35,30 +45,35 @@ class RMP{
         }
     }
 
-    stage(object, id = null){
-        let id=id || utils.getRandomNumber();
-        message={
+    stage(object, identifier){
+        let id = identifier || utils.getRandomNumber();
+        console.log("bhutagorilla",id)
+        let message={
             payload:object,
             rmpHeader:{
                 id:id
             }
         }
+        console.log("setting id=>message mapping ",id+","+JSON.stringify(message,null,4))
         this.idMessageMapping.set(id, message)
-        return id;
+        
+        return id
     }
 
     isPrimitive(object){
         return typeof object == "boolean" || typeof object == "string" || typeof object == "number" || object == null || object == undefined;
     }
 
-    getReference(id, path = []){
-        path.shift[id]
+    getReference(message, path = []){
+        path.shift(message._rmpid_)
         return {
             _rmpref_: utils.getHash(path)
         }
     }
 
     send(id){
+        console.log(id)
+        console.log("meowww",this.idMessageMapping.get(id))
         this.adapter.send(this.idMessageMapping.get(id))
     }
 
@@ -70,47 +85,64 @@ class RMP{
 export default RMP;
 
 
-// const complexTestData = {
-//     // 1. Standard Nested Object
-//     metadata: {
-//         id: 500,
-//         flags: {
-//             isVerified: true,
-//             priority: "high"
-//         }
-//     },
-//     // 2. Mixed Array (Primitives + Objects + Nested Arrays)
-//     logs: [
-//         "system_start",
-//         { event: "login", code: 200 },
-//         [10, 20, [30]] 
-//     ],
-//     // 3. Edge Case: Empty structures
-//     emptyStates: {
-//         emptyObj: {},
-//         emptyArr: []
-//     },
-//     // 4. Different Primitive types (Strings, Numbers)
-//     values: {
-//         score: 98.5,
-//         label: "test-run",
-//         zero: 0,
-//         emptyString: ""
-//     },
-//     // 5. Null (to test your !object check)
-//     nullable: null,
-//     // 6. Deeply nested path
-//     a: { b: { c: { d: "depth-check" } } }
-// };
+// // const complexTestData = {
+// //     // 1. Standard Nested Object
+// //     metadata: {
+// //         id: 500,
+// //         flags: {
+// //             isVerified: true,
+// //             priority: "high"
+// //         }
+// //     },
+// //     // 2. Mixed Array (Primitives + Objects + Nested Arrays)
+// //     logs: [
+// //         "system_start",
+// //         { event: "login", code: 200 },
+// //         [10, 20, [30]] 
+// //     ],
+// //     // 3. Edge Case: Empty structures
+// //     emptyStates: {
+// //         emptyObj: {},
+// //         emptyArr: []
+// //     },
+// //     // 4. Different Primitive types (Strings, Numbers)
+// //     values: {
+// //         score: 98.5,
+// //         label: "test-run",
+// //         zero: 0,
+// //         emptyString: ""
+// //     },
+// //     // 5. Null (to test your !object check)
+// //     nullable: null,
+// //     // 6. Deeply nested path
+// //     a: { b: { c: { d: "depth-check" } } }
+// // };
 
-const message=1;
-//To run the test:
-const rmp = new RMP({ adapter: {} });
-console.log(message==rmp.stage(message))
-// const path = ["1"];
+// const message=1;
+// //To run the test:
+// const rmp = new RMP({ adapter: {} });
+// console.log(message==rmp.stage(message))
+// // const path = ["1"];
 
-// rmp.parse(["abcdefg",1,null,true,false], path);
+// // rmp.parse(["abcdefg",1,null,true,false], path);
 
-// // Check the results
-// console.log("Mapped Hashes:");
-// console.log(rmp.hashValueMapping);
+// // // Check the results
+// // console.log("Mapped Hashes:");
+// // console.log(rmp.hashValueMapping);
+
+
+// let m1 = {
+//     name:[{
+//         name:rmp.getReference(m3),
+//         age:"male",
+//         gender:21
+//     }]
+// }
+
+// let m1id = rmp.stage(m1)
+
+// let m2 = {
+//     name:rmp.getReference(m1id,["name",0,"gender"])
+// }
+
+// rmp.stage(m1, m1id)
---
+++ test.js
@@ -0,0 +1,45 @@
+import Communication from "./communication/socket.js";
+import RMP from "./protocol/RMP.js";
+
+let params = process.argv;
+
+let communication = new Communication({
+    port:params[2],
+    serverIp:params[3],
+    serverPort:params[4]
+})
+
+let rmp = new RMP({
+    adapter:communication
+})
+
+if(params[2] == 3000){
+    setTimeout(()=>{
+        let message1 = {
+            name:"maaz"
+        }
+
+        let m1id = rmp.stage(message1);
+        console.log("m1id",m1id)
+
+        let message2 = {
+            name:rmp.getReference(m1id,["name"])
+        }
+
+        let m2id = rmp.stage(message2);
+
+        let message3 = {
+            friendName:rmp.getReference(m2id,["name"])
+        }
+
+        let m3id = rmp.stage(message3)
+
+        let message3a = {
+            _rmpid_:m3id,
+            ...message3
+        }
+        rmp.send(m1id)
+        rmp.send(m2id)
+        rmp.send(m3id)
+    },7000)
+}
